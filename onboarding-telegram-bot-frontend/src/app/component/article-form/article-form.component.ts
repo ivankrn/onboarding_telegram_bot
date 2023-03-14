5788f80809d864efcd601119823c2e309e4850da
ivankrn <kropotiniv@yandex.ru>
2023-03-14 18:20:32 +0500

    Добавлено отображение тем при добавлении статьи

--- onboarding-telegram-bot-frontend/src/app/component/article-form/article-form.component.ts
+++ onboarding-telegram-bot-frontend/src/app/component/article-form/article-form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Article } from 'src/app/model/article';
+import { ArticleTopicService } from 'src/app/service/article-topic.service';
 import { ArticleService } from 'src/app/service/article.service';
 
 @Component({
@@ -11,9 +12,12 @@ import { ArticleService } from 'src/app/service/article.service';
 export class ArticleFormComponent {
 
   article: Article;
+  articleTopics: Map<number, string>;
 
-  constructor(private route: ActivatedRoute, private router: Router, private articleService: ArticleService) {
+  constructor(private route: ActivatedRoute, private router: Router, 
+    private articleService: ArticleService, private articleTopicService: ArticleTopicService) {
     this.article = new Article();
+    this.updateTopics();
   }
 
   public onSubmit() {
@@ -22,5 +26,11 @@ export class ArticleFormComponent {
 
   public goToArticleList() {
     this.router.navigate(['/articles']);
+  }
+
+  updateTopics() {
+    this.articleTopicService.getTopicsNames().subscribe(data => {
+      this.articleTopics = data;
+    });
   }
 }
