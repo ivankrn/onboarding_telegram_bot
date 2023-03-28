50a258114440c08bb7c20467cd2ecdbd5b2665da
ivankrn <kropotiniv@yandex.ru>
2023-03-28 15:33:52 +0500

    Переход на реактивные формы

--- onboarding-telegram-bot-frontend/src/app/app.module.ts
+++ onboarding-telegram-bot-frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { ReactiveFormsModule }   from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +23,7 @@ import { DashboardComponent } from './component/dashboard/dashboard/dashboard.co
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   providers: [ArticleService],
   bootstrap: [AppComponent]
--- onboarding-telegram-bot-frontend/src/app/component/article-form/article-form.component.ts
+++ onboarding-telegram-bot-frontend/src/app/component/article-form/article-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { map, Observable } from 'rxjs';
 import { Article } from 'src/app/model/article';
@@ -14,6 +15,16 @@ export class ArticleFormComponent implements OnInit {
 
   article: Article;
   articleTopics: Observable<Map<number, string>>;
+  form = new FormGroup({
+    title: new FormControl("", Validators.required),
+    content: new FormControl("", Validators.required),
+    topic: new FormGroup({
+      id: new FormControl(null, Validators.required),
+      title: new FormControl()
+    }),
+    usefulLinks: new FormControl(""),
+    testLink: new FormControl("")
+  });
 
   constructor(private route: ActivatedRoute, private router: Router, 
     private articleService: ArticleService, private articleTopicService: ArticleTopicService) {
@@ -22,10 +33,14 @@ export class ArticleFormComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.route.paramMap.pipe(map(() => window.history.state)).subscribe(data => this.article = data);
+    this.route.paramMap.pipe(map(() => window.history.state)).subscribe(data => {
+      this.article = data;
+      this.form.patchValue(data);
+    });
   }
 
   public onSubmit() {
+    this.article = Object.assign(this.article, this.form.value);
     this.articleService.save(this.article).subscribe(() => this.goToArticleList());
   }
 
