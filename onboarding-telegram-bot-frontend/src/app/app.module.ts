6bcf71b1956f67155d4b13808b3ad0faecf0d0a5
ivankrn <kropotiniv@yandex.ru>
2023-04-22 14:53:21 +0500

    Убраны лишние поддиректории

--- onboarding-telegram-bot-frontend/src/app/app.module.ts
+++ onboarding-telegram-bot-frontend/src/app/app.module.ts
@@ -4,17 +4,17 @@ import { ReactiveFormsModule }   from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ArticleListComponent } from './component/article-list/article-list/article-list.component';
+import { ArticleListComponent } from './component/article-list/article-list.component';
 import { ArticleFormComponent } from './component/article-form/article-form.component';
 import { ArticleService } from './service/article.service';
 import { HttpClientModule } from '@angular/common/http';
-import { TestListComponent } from './component/test-list/test-list/test-list.component';
-import { StatisticsComponent } from './component/statistics/statistics/statistics.component';
-import { TestFormComponent } from './component/test-form/test-form/test-form.component';
+import { TestListComponent } from './component/test-list/test-list.component';
+import { StatisticsComponent } from './component/statistics/statistics.component';
+import { TestFormComponent } from './component/test-form/test-form.component';
 import { PaginationModule } from './pagination/pagination/pagination.module';
 import { TestService } from './service/test.service';
-import { TestStatisticsComponent } from './component/statistics/statistics/test-statistics/test-statistics.component';
-import { ArticleStatisticsComponent } from './component/statistics/statistics/article-statistics/article-statistics.component';
+import { TestStatisticsComponent } from './component/statistics/test-statistics/test-statistics.component';
+import { ArticleStatisticsComponent } from './component/statistics/article-statistics/article-statistics.component';
 
 @NgModule({
   declarations: [
