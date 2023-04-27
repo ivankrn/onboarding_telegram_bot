6882e18517c1aa7bce66d773198e08fd12db6e47
ivankrn <kropotiniv@yandex.ru>
2023-04-27 19:39:47 +0500

    Добавлена кнопка для скрытия / раскрытия бокового меню

--- onboarding-telegram-bot-frontend/src/app/app.component.ts
+++ onboarding-telegram-bot-frontend/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Onboarding Telegram bot';
+
+  isSidebarOpen: boolean = true;
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
