65649d17a2f7722a1a6c34dbc033ad44334cc124
ivankrn <kropotiniv@yandex.ru>
2023-04-29 11:37:43 +0500

    Правки конфигурации тестов

--- onboarding-telegram-bot-backend/src/test/java/com/ppteam/onboardingtelegrambot/OnboardingTelegramBotApplicationTests.java
+++ onboarding-telegram-bot-backend/src/test/java/com/ppteam/onboardingtelegrambot/OnboardingTelegramBotApplicationTests.java
@@ -3,7 +3,7 @@ package com.ppteam.onboardingtelegrambot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = OnboardingTelegramBotApplicationTests.class)
 class OnboardingTelegramBotApplicationTests {
 
 	@Test
