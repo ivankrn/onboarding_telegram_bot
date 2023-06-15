182da5f6f06e97a0781cb4f4b2c3955ff6fc145e
ivankrn <kropotiniv@yandex.ru>
2023-06-15 08:28:52 +0500

    Правки по редизайну

--- onboarding-telegram-bot-frontend/src/app/app.component.ts
+++ onboarding-telegram-bot-frontend/src/app/app.component.ts
@@ -9,8 +9,24 @@ export class AppComponent {
   title = 'Onboarding Telegram bot';
 
   isSidebarOpen: boolean = false;
+  isSidebarMobileOpen: boolean = false;
 
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  screenWidth = window.innerWidth;
+
+  toggleSidebarMobile() {
+    if (this.screenWidth < 800) {
+      this.isSidebarMobileOpen = !this.isSidebarMobileOpen;
+    }
+  }
+
+  toggleSidebarMobileButtons() {
+    if (this.screenWidth < 800) {
+      this.isSidebarMobileOpen = !this.isSidebarMobileOpen;
+      this.isSidebarOpen = !this.isSidebarOpen;
+    }
   }
 }
